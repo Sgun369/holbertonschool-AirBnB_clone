1cc4e03d2f5d74d7a8ac46cb942eb855fef4249e
Sgun369 <souhaiebjebali@gmail.com>
2023-11-04 18:56:20 +0100

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
-
 
     def __str__(self):
         """
