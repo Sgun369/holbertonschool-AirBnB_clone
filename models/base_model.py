6a4ce69c67f54e6162ca49839c765ac4ffabe6ad
Sgun369 <souhaiebjebali@gmail.com>
2023-11-04 10:58:00 +0100

    __init__

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ base_model module: Defines the BaseModel class.
 import models
 import uuid
 from datetime import datetime
+from models.engine.file_storage import storage
 
 
 class BaseModel:
@@ -30,6 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -46,6 +48,7 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
