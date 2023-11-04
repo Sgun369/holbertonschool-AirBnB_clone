d5b6cda03150540ad93aa0656e9a5d2bbe467187
Sgun369 <souhaiebjebali@gmail.com>
2023-11-04 18:54:11 +0100

    all

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,24 @@ class BaseModel:
     BaseModel class: Represents a base model with common attributes and methods
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
         It assigns a unique ID, creation time, and update time to the instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
+
 
     def __str__(self):
         """
