71a947d16d805bcbfd39225e7bec562cf1ba45f2
Sgun369 <souhaiebjebali@gmail.com>
2023-11-04 18:29:50 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -40,11 +40,8 @@ class BaseModel:
         Returns:
             dict: A dictionary containing the object's attributes and metadata.
         """
-        return {
-            'my_number': self.__dict__.get('my_number'),
-            'name': self.__dict__.get('name'),
-            '__class__': self.__class__.__name__,
-            'updated_at': self.updated_at.isoformat(),
-            'id': self.id,
-            'created_at': self.created_at.isoformat()
-        }
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
