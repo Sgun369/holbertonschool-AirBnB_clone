80b9b0ca5c7d80327499088e08636355082a2412
Sgun369 <souhaiebjebali@gmail.com>
2023-11-04 17:59:18 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ base_model module: Defines the BaseModel class.
 import models
 import uuid
 from datetime import datetime
-from models.engine.file_storage import storage
 
 
 class BaseModel:
@@ -13,42 +12,28 @@ class BaseModel:
     BaseModel class: Represents a base model with common attributes and methods
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
         Initializes a new instance of the BaseModel class.
         It assigns a unique ID, creation time, and update time to the instance.
         """
-        if kwargs:
-            if "__class__" in kwargs:
-                kwargs.pop("__class__")
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    if isinstance(value, str):
-                        value = datetime.strptime(value,
-                                                  "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
-        Returns a string representation of the BaseModel object.
-        Format: "[ClassName] (id) {attribute_dict}"
+        Returns a string representation of the BaseModel object
         """
 
         class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
         Updates the 'updated_at' attribute with the current datetime.
         """
-
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """
