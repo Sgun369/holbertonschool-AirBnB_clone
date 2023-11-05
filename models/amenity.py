4c923078167a82403571a239cedffcd3cf1b3bec
maouia <maouiabend@gmail.com>
2023-11-05 19:19:50 +0100

    create class amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Amenity module: Contains the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class: Represents an amenity with a name."""
+    name = ""
