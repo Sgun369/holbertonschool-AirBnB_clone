9f836a2871cb6ef683cdf90aecdf7c6ff23950ef
Sgun369 <souhaiebjebali@gmail.com>
2023-11-05 21:43:30 +0100

    fic test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,57 +1,64 @@
-#!/usr/bin/python3
-from console import HBNBCommand
 import unittest
+from unittest.mock import patch
+from io import StringIO
 import sys
-sys.path.append('../..')
+from console import HBNBCommand
 
 
-class TestHBNBCommand(unittest.TestCase):
-    """Test suite for HBNBcommand functionality"""
+class TestHBNBCommandMethods(unittest.TestCase):
     def setUp(self):
-        """Set up test environment"""
+        """Set up the HBNBCommand instance for testing"""
         self.console = HBNBCommand()
 
     def tearDown(self):
-        """Tear down after test execution"""
+        """Tear down after testing"""
         pass
 
-    def test_quit(self):
-        """Test quit command"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.assertTrue(self.console.onecmd("quit"))
-            self.assertEqual(fake_out.getvalue(), "")
+    def capture_output(self, func, *args):
+        """Capture the output of the function execution"""
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        func(*args)
+        sys.stdout = sys.__stdout__
+        return captured_output.getvalue().strip()
 
-    def test_EOF(self):
-        """Test the EOF command"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.assertTrue(self.console.onecmd("EOF"))
-            self.assertEqual(fake_out.getvalue(), "")
+    def test_quit_command(self):
+        """Test quit command"""
+        self.assertTrue(self.console.do_quit(""))
+
+    def test_EOF_command(self):
+        """Test EOF command"""
+        self.assertTrue(self.console.do_EOF(""))
 
     def test_emptyline(self):
-        """Test an emptyy line input"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.console.onecmd("")
-            self.assertEqual(fake_out.getvalue(), "")
+        """Test emptyline method"""
+        self.assertIsNone(self.console.emptyline())
 
     def test_help_quit(self):
-        """Test the help quit command"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.console.onecmd("help quit")
-            self.assertEqual(fake_out.getvalue(),
-                             "Quit command to exit the program\n\n")
+        """Test help for quit command"""
+        captured_output = self.capture_output(self.console.help_quit)
+        self.assertEqual(captured_output, "Quit command to exit the program")
 
-    def test_do_create_missing_class(self):
+    def test_create_missing_class_name(self):
         """Test create command with missing class name"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.console.onecmd("create")
-            self.assertEqual(fake_out.getvalue(), "** class name missing **\n")
+        captured_output = self.capture_output(self.console.do_create)
+        self.assertEqual(captured_output, "** class name missing **")
 
-    def test_do_create_invalid_class(self):
-        """Test create command with an invalid class name"""
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            self.console.onecmd("create InvalidClass")
-            self.assertEqual(fake_out.getvalue(),
-                             "** class doesn't exist **\n")
+    def test_create_invalid_class_name(self):
+        """Test create command with invalid class name"""
+        captured_output = self.capture_output(
+            self.console.do_create, "InvalidClassName")
+        self.assertEqual(captured_output, "** class doesn't exist **")
+
+    def test_create_valid_class(self):
+        """Test create command with valid class name"""
+        captured_output = self.capture_output(self.console.do_create, "User")
+        self.assertTrue(captured_output)  # Ensure that an ID is generated
+
+    def test_show_missing_class_name(self):
+        """Test show command with missing class name"""
+        captured_output = self.capture_output(self.console.do_show, "")
+        self.assertEqual(captured_output, "** class name missing **")
 
 
 if __name__ == '__main__':
