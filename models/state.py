7fb5469e825a7b94e1c940b1cff908a83a434432
maouia <maouiabend@gmail.com>
2023-11-05 19:19:18 +0100

    create class state

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""State module: Contains the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class: Represents a state with a name."""
+    name = ""
