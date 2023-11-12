921b03f272f26b9dd962bb523b13318b36ece88d
maouia <maouiabend@gmail.com>
2023-11-12 03:21:52 +0100

    fix file storage comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,19 +32,16 @@ class FileStorage:
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         odict = FileStorage.__objects
-        # Serialize objects to a dictionary
         objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f, indent=2)  # Write the serialized data to the file
+            json.dump(objdict, f, indent=2)
 
     def reload(self):
         """Deserializes the JSON file to instances in __objects."""
         try:
             with open(self.__file_path, 'r', encoding="UTF8") as f:
-                loaded_data = json.load(f)  # Load JSON data from the file
-                for key, value in loaded_data.items(
-                ):  # Iterate over the deserialized data
-                    # Extract the object's ID from the key
+                loaded_data = json.load(f)
+                for key, value in loaded_data.items():
                     obj_id = key.split(".")[1]
                     # Extract and remove the class name
                     obj_class = value.pop("__class__")
@@ -54,4 +51,4 @@ class FileStorage:
                     # Store the instance in __objects
                     self.__objects[key] = obj_instance
         except FileNotFoundError:
-            pass  # Handle the case when the file is not found
+            pass  # file is not found
